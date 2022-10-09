52d6a7c964074ecdcf190205e999f7e7cda5203f
Sumanta Kabiraj <sumantablog@gmail.com>
2022-10-09 13:29:23 +0530

    CRUD App Using React Hooks Complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 function App() {
+  let [name, setName] = useState("");
+  let [username, setUserName] = useState("");
+  let [data, setData] = useState(
+    JSON.parse(localStorage.getItem("data")) || []
+  );
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (name && username) {
+      let obj = { name, username };
+      setData(data.concat(obj));
+      setName("");
+      setUserName("");
+      localStorage.setItem("data", JSON.stringify(data.concat(obj)));
+    }
+  };
+
+  const handleChange = ({ target }) => {
+    let { id } = target.dataset;
+    if (id === "name") {
+      setName(target.value);
+    }
+    if (id === "username") {
+      setUserName(target.value);
+    }
+  };
+
+  const handleDelete = (id) => {
+    let newData = [...data];
+    newData.splice(id, 1);
+    setData(newData);
+    localStorage.setItem("data", JSON.stringify(newData));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section>
+        <h1>Users List</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            value={name}
+            type="text"
+            placeholder="Enter Name"
+            onChange={handleChange}
+            data-id="name"
+          />
+          <input
+            value={username}
+            type="text"
+            placeholder="Enter Username"
+            onChange={handleChange}
+            data-id="username"
+          />
+          <button>Add User</button>
+        </form>
+        <div className="grid display">
+          {data.length === 0 ? (
+            <h2>No Users to display</h2>
+          ) : (
+            data.map((d, i) => {
+              return (
+                <div className="box" key={d.username}>
+                  <div>
+                    <h2>Name:{d.name}</h2>
+                    <h2>Username: {d.username}</h2>
+                  </div>
+                  <div>
+                    <Link
+                      to={{
+                        pathname: `/edit/${d.username}`,
+                        id: i,
+                      }}
+                    >
+                      <button className="edit">Edit</button>
+                    </Link>
+                    <button className="delete" onClick={() => handleDelete(i)}>
+                      Delete
+                    </button>
+                  </div>
+                </div>
+              );
+            })
+          )}
+        </div>
+      </section>
+    </>
   );
 }
 
---
+++ src/EditUser.js
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import { withRouter } from "react-router-dom";
+
+function EditUser(props) {
+  let id = props.location.id;
+
+  let [name, setName] = useState(
+    JSON.parse(localStorage.getItem("data"))[id].name
+  );
+
+  let [username, setUserName] = useState(
+    JSON.parse(localStorage.getItem("data"))[id].username
+  );
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    let data = JSON.parse(localStorage.getItem("data"));
+    let obj = { name, username };
+    data[id] = obj;
+    localStorage.setItem("data", JSON.stringify(data));
+    props.history.push("/");
+  };
+
+  const handleChange = ({ target }) => {
+    let { id } = target.dataset;
+    if (id === "name") {
+      setName(target.value);
+    }
+    if (id === "username") {
+      setUserName(target.value);
+    }
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <h1>Edit User</h1>
+        <input
+          value={name}
+          type="text"
+          placeholder="Enter Name"
+          data-id="name"
+          onChange={handleChange}
+        />
+        <input
+          value={username}
+          type="text"
+          placeholder="Enter Username"
+          data-id="username"
+          onChange={handleChange}
+        />
+        <button>Update User</button>
+      </form>
+    </>
+  );
+}
+
+export default withRouter(EditUser);
